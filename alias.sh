37b91cac4a95ee65931174be23c13450ff4eb9e6
Shubham Tandon <iamshubham316@gmail.com>
2024-10-08 09:29:38 +0000

    add(alias): added commands with arguments

--- alias.sh
+++ alias.sh
@@ -5,3 +5,20 @@ alias nrd='npm run dev'
 alias zzz='shutdown -h now'
 alias gaa='git add .'
 alias gpsh='git push'
+alias zz='exit'
+
+gplo() {
+    if [ -z "$1" ]; then
+        echo "Please provide a branch name."
+    else
+        git pull origin "$1"
+    fi
+}
+gcm() {
+    if [ -z "$1" ]; then
+        echo "Please provide a commit message."
+    else
+        git commit -m "$1"
+    fi
+}
+
