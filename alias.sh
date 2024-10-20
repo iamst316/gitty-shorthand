5689731e0e8c9a1f8b783f37f809a89ca5a072e3
Shubham Tandon <iamshubham316@gmail.com>
2024-10-20 13:43:21 +0000

    add(alias): alias for git clone

--- alias.sh
+++ alias.sh
@@ -10,24 +10,32 @@ alias gbr='git branch'
 alias gbra='git branch -a'
 
 gplo() {
-    if [ -z "$1" ]; then
-        echo "Please provide a branch name."
-    else
-        git pull origin "$1"
-    fi
+  if [ -z "$1" ]; then
+    echo "Please provide a branch name."
+  else
+    git pull origin "$1"
+  fi
 }
 gcm() {
-    if [ -z "$1" ]; then
-        echo "Please provide a commit message."
-    else
-        git commit -m "$1"
-    fi
+  if [ -z "$1" ]; then
+    echo "Please provide a commit message."
+  else
+    git commit -m "$1"
+  fi
 }
 
 gclo() {
-    if [ -z "$1" ]; then
-        echo "Please provide a repository url."
-    else
-        git commit -m "$1"
-    fi
+  if [ -z "$1" ]; then
+    echo "Please provide a repository url."
+  else
+    git commit -m "$1"
+  fi
+}
+
+gcln() {
+  if [ -z "$1" ]; then
+    echo "Please provide a repository url."
+  else
+    git clone "$1"
+  fi
 }
